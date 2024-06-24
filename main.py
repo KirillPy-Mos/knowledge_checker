ba9a613fbcebbddae3e47c39c76706d00afff3a0
FoKas WorkStation North <kirillkasparyants@yandex.com>
2024-06-24 23:21:28 +0300

    start to write function login()

--- main.py
+++ main.py
@@ -6,19 +6,43 @@ reg_status_set = open("reg.txt", "w")
 reg_status_get = open("reg.txt", "r")
 if reg_status_get:
     u_d = open("user_data1.csv", "r")
+    reg_status = True
 else:
     u_d = open("user_data1.csv", "w")
 
 # vars
-subject = ""
+subject_user = ""
 theme = 0
 question = 0
 all_questions = 0
 percent_correct = 0
 correct_answers = 0
 incorrect_answers = 0
-name = ""
-mode = ""
+name_user = ""
+class_user = 0
 setter_name = ""
 setter_subject = ""
-setter_mode = ""
+setter_class = 0
+formule_for_set = "name,subject,class"
+formule_for_get = ["name", "subject", "class"]
+
+#functions
+
+while True:
+    def login():
+        print("Вход в систему, подождите...")
+        if reg_status:
+            data_user = csv.reader(u_d)
+            data_user_list = []
+            for data in data_user:
+                data_user_list.append(data)
+            for i in range(0, 3):
+                if formule_for_get[i] in data_user_list:
+                    data_user_list.remove(formule_for_get[i])
+            global name_user, subject_user, class_user
+            name_user = data_user_list[0]
+            subject_user = data_user_list[1]
+            class_user = data_user_list[2]
+            print("Вход выполнен. Здраствуйте,", name_user, "!")
+
+
